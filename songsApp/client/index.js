be279663cb8091cd53b982382bfbb2243f9f1a7b
abidex4yemi <abidex4yemi@gmail.com>
2019-12-29 10:35:02 +0100

    Implement routing for songs

--- songsApp/client/index.js
+++ songsApp/client/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 
+import SongList from "./components/SongList";
+
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: "http://localhost:5000/"
+  uri: ""
 });
 
 const client = new ApolloClient({
@@ -18,7 +21,9 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <Router>
+        <Route exact path="/" render={props => <SongList {...props} />} />
+      </Router>
     </ApolloProvider>
   );
 };
