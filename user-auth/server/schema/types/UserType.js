0523d4dbbde1955c53c9af3df5c7cd034f58d575
abidex4yemi <abidex4yemi@gmail.com>
2020-01-02 20:04:39 +0100

    Create user types

---
+++ user-auth/server/schema/types/UserType.js
@@ -0,0 +1,17 @@
+const graphql = require("graphql");
+
+const { GraphQLObjectType, GraphQLID, GraphQLString } = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: "UserType",
+  fields: {
+    id: {
+      type: GraphQLID
+    },
+    email: {
+      type: GraphQLString
+    }
+  }
+});
+
+module.exports = UserType;
