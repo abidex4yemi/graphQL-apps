e4b5f3162ed4214ba7403e211769c087e5021930
abidex4yemi <abidex4yemi@gmail.com>
2020-01-02 22:37:13 +0100

    Implement auth services

--- user-auth/server/services/auth.js
+++ user-auth/server/services/auth.js
@@ -41,3 +41,47 @@ passport.use(
     });
   })
 );
+
+const signup = ({ email, password, req }) => {
+  if (!email && !password) {
+    throw new Error("You must provide email and password");
+  }
+  const user = new User({ email, password });
+
+  return User.findOne({ email })
+    .then(existingUser => {
+      if (existingUser) {
+        throw new Error("Email already exist");
+      }
+
+      return user.save();
+    })
+    .then(user => {
+      return new Promise((resolve, reject) => {
+        req.logIn(user, err => {
+          if (err) {
+            reject(err);
+          }
+
+          resolve(user);
+        });
+      });
+    });
+};
+
+const login = ({ email, password, req }) => {
+  return new Promise((resolve, reject) => {
+    passport.authenticate("local", (err, user) => {
+      if (!user) {
+        reject("Invalid credentials");
+      }
+
+      req.login(user, () => resolve(user));
+    })({ body: { email, password } });
+  });
+};
+
+module.exports = {
+  signup,
+  login
+};
