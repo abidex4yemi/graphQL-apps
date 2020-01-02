2adc17305f08c6ebdcc41bc4d25cccc10b527deb
abidex4yemi <abidex4yemi@gmail.com>
2020-01-02 11:53:48 +0100

    Implement likes feature

--- songsApp/client/src/components/LyricList.js
+++ songsApp/client/src/components/LyricList.js
@@ -1,14 +1,40 @@
 import React from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 const LyricList = props => {
+  const handleLike = (id, likes) => {
+    props.mutate({
+      variables: {
+        id
+      },
+      optimisticResponse: {
+        __typename: "Mutation",
+        likeLyric: {
+          id,
+          __typename: "LyricType",
+          likes: likes + 1
+        }
+      }
+    });
+  };
+
   const renderLyrics = () => {
     if (!props.lyrics.length) {
       return <div>No lyric yet</div>;
     }
 
-    return props.lyrics.map(({ content, id }) => (
+    return props.lyrics.map(({ content, id, likes }) => (
       <li className="collection-item" key={id}>
         {content}
+        <small className="right"> {likes}</small>
+        <i
+          className="material-icons right"
+          style={{ color: "blue", cursor: "pointer" }}
+          onClick={() => handleLike(id, likes)}
+        >
+          thumb_up
+        </i>
       </li>
     ));
   };
@@ -22,4 +48,13 @@ const LyricList = props => {
   );
 };
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
