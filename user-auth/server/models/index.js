70b9b60325c88e1bb842f7077f75247488c99873
abidex4yemi <abidex4yemi@gmail.com>
2020-01-02 19:35:54 +0100

    Install project dependencies

---
+++ user-auth/server/models/index.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+const mongoServer = new MongoMemoryServer();
+
+mongoose.Promise = Promise;
+
+const connectDB = () => {
+  mongoServer.getConnectionString().then(mongoUri => {
+    // options for mongoose above 4.11.3 and above
+
+    mongooseOPTS = {
+      autoReconnect: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000
+    };
+
+    mongoose.connect(mongoUri, mongooseOPTS);
+
+    mongoose.connection.on("error", err => {
+      if (err.message.code == "ETIMEDOUT") {
+        console.log(err);
+        mongoose.connect(mongoUri, mongooseOPTS);
+      }
+      console.log(e);
+    });
+
+    mongoose.connection.once("open", () => {
+      console.log("MongoDB successfully connected");
+    });
+  });
+};
+
+module.exports = {
+  connectDB
+};
