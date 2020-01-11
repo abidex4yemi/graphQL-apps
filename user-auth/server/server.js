1663b0d27a68993d9f84b7f08cd731dc1981bddd
abidex4yemi <abidex4yemi@gmail.com>
2020-01-11 14:14:04 +0100

    Fix: database connection

--- user-auth/server/server.js
+++ user-auth/server/server.js
@@ -1,69 +1,42 @@
-require("dotenv").config();
-require("./models/user");
-const express = require("express");
-const cors = require("cors");
-const expression = require("express-session");
-const passport = require("passport");
-const MongoStore = require("connect-mongo")(expression);
-const bodyParser = require("body-parser");
-const expressGraphQL = require("express-graphql");
-const mongoose = require("mongoose");
-const { MongoMemoryServer } = require("mongodb-memory-server");
+require("dotenv").config()
+const express = require('express');
+const models = require('./models/user');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo')(session);
+const schema = require('./schema');
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 
-const mongoServer = new MongoMemoryServer();
+mongoose.Promise = global.Promise;
 
-mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-(async () => {
-  let mongoUri = await mongoServer.getUri();
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
-  const mongooseOpts = {
-    // options for mongoose 4.11.3 and above
-    autoReconnect: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 1000
-  };
-
-  await mongoose.connect(mongoUri, mongooseOpts);
-
-  mongoose.connection.on("error", e => {
-    if (e.message.code === "ETIMEDOUT") {
-      mongoose.connect(mongoUri, mongooseOpts);
-    }
-  });
-
-  mongoose.connection.once("open", () => {
-    console.log(`MongoDB successfully connected to ${mongoUri}`);
-  });
-
-  app.use(
-    expression({
-      resave: true,
-      saveUninitialized: true,
-      secret: process.env.COOKIE_KEY,
-      store: new MongoStore({
-        url: mongoUri,
-        autoReconnect: true
-      })
-    })
-  );
-
-  app.use(passport.initialize());
-  app.use(passport.session());
-})();
-
-const schema = require("./schema");
-
-app.use(
-  "/graphql",
-  expressGraphQL({
-    schema,
-    graphiql: true
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.COOKIE_KEY,
+  store: new MongoStore({
+    url: process.env.MONGO_URI,
+    autoReconnect: true
   })
-);
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
 
 module.exports = app;
