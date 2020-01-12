5a150f34ba48d0ea4d84bccea529060018ccf222
abidex4yemi <abidex4yemi@gmail.com>
2020-01-12 11:02:31 +0100

    Set up user auth client side

--- user-auth/client/src/index.js
+++ user-auth/client/src/index.js
@@ -1,5 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./components/App";
+
+const cache = new InMemoryCache();
+
+const link = new HttpLink({
+  uri: "http://localhost:5000/graphql"
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+  dataIdFromObject: o => o.id
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
